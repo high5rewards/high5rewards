d627d8333fc563ff2d9b186cc1d9277ce5ee1ed5
paigexx <pcjones12@gmail.com>
2022-02-03 18:09:40 -0600

    configured moralis

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {MoralisProvider} from "react-moralis";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const appId = process.env.REACT_APP_APP_ID 
+const serverUrl = process.env.REACT_APP_SERVER_URL 
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MoralisProvider appId={appId} serverUrl={serverUrl}>
+      <App />
+    </ MoralisProvider >
   </React.StrictMode>,
   document.getElementById('root')
 );
