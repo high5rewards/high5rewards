b589e71f7af585d7d1657a1310e319b9092bb03b
lenoot.sol <lenoot.sol@protonmail.com>
2022-02-04 16:32:15 -0600

    Prototype logo and favicon

--- client/src/routes/Landing/Landing.js
+++ client/src/routes/Landing/Landing.js
@@ -7,11 +7,11 @@ import Landing_nav_top from "../../components/landing_nav_top"
 import Login_bttn from "../../components/login_bttn"
 
 
-
 function Landing() {
   return (
     <div>
         <Landing_nav_top />
+        <img id={styles.logo} src={require('../../images/temp-logo.png')} alt='logo'/>
         <h1>This is the landing page</h1>
         <Login_bttn />
         <Landing_nav_bottom />
