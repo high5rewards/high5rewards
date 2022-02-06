de2517bf9a4afc94abf9851581f4dfe6a54fdfc0
lenoot.sol <lenoot.sol@protonmail.com>
2022-02-05 22:17:26 -0600

    Isolate MintButton components

--- client/src/routes/VendorDashboard/components/MintButton.js
+++ client/src/routes/VendorDashboard/components/MintButton.js
@@ -1,15 +1,15 @@
-import { useState } from "react"
-import { ethers } from 'ethers'
-import { create as ipfsHttpClient } from 'ipfs-http-client'
-import Web3Modal from 'web3modal'
-import web3 from 'web3'
+// import { useState } from "react"
+// import { ethers } from 'ethers'
+// import { create as ipfsHttpClient } from 'ipfs-http-client'
+// import Web3Modal from 'web3modal'
+// import web3 from 'web3'
 
-import { rewardcontractaddress, marketaddress } from '../../../../../high5rewards/config'
-import Reward from '../../../../../high5rewards/artifacts/contracts/Reward.sol'
-import RewardsMarket from '../../../../../high5rewards/artifacts/contracts/RewardsMarket.sol'
+// import { rewardcontractaddress, marketaddress } from '../../../Hardhat/config'
+// import Reward from '../../../Hardhat/contracts/Reward.sol'
+// import RewardsMarket from '../../../Hardhat/contracts/RewardsMarket.sol'
 
 
-import { Button } from '@mui/material';
+// import { Button } from '@mui/material';
 
 
 
@@ -24,96 +24,93 @@ const MintButton = () => {
 
     // Create Sale
     let createSale = async(url) => {
-        const web3Modal = new Web3Modal({
-            cacheProvider: true
-        })
+        console.log(0)
+        // const web3Modal = new Web3Modal({
+        //     cacheProvider: true
+        // })
     
-        const connection = await web3Modal.connect() 
-        const provider = new ethers.providers.Web3Provider(connection)
-        const signer = provider.getSigner() 
+        // const connection = await web3Modal.connect() 
+        // const provider = new ethers.providers.Web3Provider(connection)
+        // const signer = provider.getSigner() 
         
-        let contract = new ethers.Contract(rewardcontractaddress, Reward.abi, signer) 
-        let transaction = await contract.createToken(url)
-        let tx = await transaction.wait()
-        let value = event.args[2]
-        let tokenId = value.toNumber()
-        const price = web3.utils.toWei(formInput.price, 'ether')
+        // let contract = new ethers.Contract(rewardcontractaddress, Reward.abi, signer) 
+        // let transaction = await contract.createToken(url)
+        // let tx = await transaction.wait()
+        // let value = event.args[2]
+        // let tokenId = value.toNumber()
+        // const price = web3.utils.toWei(formInput.price, 'ether')
 
-        const listingPrice = web3.utils.toWei('0.1', 'ether')
+        // const listingPrice = web3.utils.toWei('0.1', 'ether')
 
-        contract = new ethers.Contract(marketaddress, RewardsMarket.abi, signer)
-        transaction = await contract.createMarketItem(rewardcontractaddress, tokenId, price, { value: listingPrice })
+        // contract = new ethers.Contract(marketaddress, RewardsMarket.abi, signer)
+        // transaction = await contract.createMarketItem(rewardcontractaddress, tokenId, price, { value: listingPrice })
         
-        await transaction.wait()
+        // await transaction.wait()
     }
 
     // On Change
     async function onChange(e) {
-        const file = e.target.files[0];
-        try {
-          const added = await client.add(
-            file,
-            {
-              progress: (prog) => console.log(`received: ${prog}`)
-            }
-          )
-          const url = `https://ipfs.infura.io/ipfs/${added.path}`
-          setFileUrl(url)
-        } catch (error) {
-          console.log('Error uploading file: ', error);
-        }  
+        console.log(0)
+
+        // const file = e.target.files[0];
+        // try {
+        //   const added = await client.add(
+        //     file,
+        //     {
+        //       progress: (prog) => console.log(`received: ${prog}`)
+        //     }
+        //   )
+        //   const url = `https://ipfs.infura.io/ipfs/${added.path}`
+        //   setFileUrl(url)
+        // } catch (error) {
+        //   console.log('Error uploading file: ', error);
+        // }  
       }
 
       // Create Market
       async function createMarket() {
-        const { name, description, price } = formInput
-        if (!name || !description || !price || !fileUrl) return
-        // first, upload to IPFS
-        const data = JSON.stringify({
-          name, description, image: fileUrl
-        })
-        try {
-          const added = await client.add(data)
-          const url = `https://ipfs.infura.io/ipfs/${added.path}`
-          createSale(url)
-        } catch (error) {
-          console.log('Error uploading file: ', error);
-        }  
+        console.log(0)
+
+        // const { name, description, price } = formInput
+        // if (!name || !description || !price || !fileUrl) return
+        // // first, upload to IPFS
+        // const data = JSON.stringify({
+        //   name, description, image: fileUrl
+        // })
+        // try {
+        //   const added = await client.add(data)
+        //   const url = `https://ipfs.infura.io/ipfs/${added.path}`
+        //   createSale(url)
+        // } catch (error) {
+        //   console.log('Error uploading file: ', error);
+        // }  
       }
 
 
 
     return (
-        <div className="flex justify-center">
-            <div className="w-1/2 flex flex-col pb-12">
+        <div>
+            <div>
                 <input 
                 placeholder="NFT Name"
-                className="mt-8 border rounded p-4"
-                onChange={e => updateFormInput({ ...formInput, name: e.target.value })}
+                onChange={e => console.log(e.target.value) }
                 />
                 <input
                 placeholder="NFT Description"
-                className="mt-2 border rounded p-4"
-                onChange={e => updateFormInput({ ...formInput, description: e.target.value })}
+                onChange={e => console.log(e.target.value) }
                 />
                 <input
                 placeholder="NFT Price in Eth"
-                className="mt-2 border rounded p-4"
-                onChange={e => updateFormInput({ ...formInput, price: e.target.value })}
+                onChange={e => console.log(e.target.value) }
                 />
                 <input
                 type="file"
                 name="NFT"
-                className="my-4"
                 onChange={onChange}
                 />
-                {
-                fileUrl && (
-                    <img className="rounded mt-4" width="350" src={fileUrl} />
-                )
-                }
-                <button onClick={createMarket} className="mt-4 bg-blue-500 text-white rounded p-4 shadow-lg">
-                Create NFT
+                <div>File URL and display image</div>
+                <button onClick={createMarket} >
+                Mint NFT
                 </button>
             </div>
         </div>
