3052a3cc975e17d047f9aece6907f8f382df1685
lenoot.sol <lenoot.sol@protonmail.com>
2022-02-16 21:25:13 -0600

    Build node server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,33 @@
 /** Node server */
+
+//init node express app
+const path = require("path");
+const express = require("express");
+let PORT = process.env.PORT;
+if (PORT === null || PORT === undefined || PORT === "") {
+  PORT = 3000;
+}
+const app = express();
+
+//
+//
+//Node serve files for built React app
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.static("../client/public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path + "index.html");
+});
+//
+//
+
+
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+// });
+
+//listen at specified port
+app.listen(PORT, () => {
+  console.log(`Server listening at ${PORT}...`);
+});
